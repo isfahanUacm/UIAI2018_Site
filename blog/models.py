4287bbafb3b94b4d641dc8eab4583efae7a5dca6
Parham Kazemi <pkazemi3@gmail.com>
2018-11-07 09:59:43 +0330

    Added blog models

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from rest_api.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.TextField(max_length=8192)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(max_length=1024)
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} - {}'.format(self.user.get_full_name(), self.post.title)
--- uiai2018_site/settings.py
+++ uiai2018_site/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_api',
+    'blog',
+    'main',
 ]
 
 MIDDLEWARE = [
