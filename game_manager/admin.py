aab9b6a5ffdf6da8ca272864cbd41c33279b6cd4
Parham Kazemi <pkazemi3@gmail.com>
2018-11-17 23:00:11 +0330

    Added game_manager models and views

--- game_manager/admin.py
+++ game_manager/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from game_manager.models import *
+
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'sender', 'receiver', 'status']
+    list_filter = ['sender', 'receiver', 'status']
+    search_fields = ['sender', 'receiver', 'status']
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'get_request_sender_team', 'get_request_receiver_team', 'status']
+    list_filter = ['status']
+
+
+admin.site.register(GameRequest, RequestAdmin)
+admin.site.register(Game, GameAdmin)
--- game_manager/views.py
+++ game_manager/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework.status import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from game_manager.models import *
+from user_panel.models import *
+
+
+@api_view(['POST'])
+def send_game_request(request):
+    try:
+        receiver = Team.objects.get(pk=int(request.data.get('team_id')))
+    except ValueError:
+        return Response({'message': 'شناسه تیم نامعتبر است.'}, status=HTTP_400_BAD_REQUEST)
+    except Team.DoesNotExist:
+        return Response({'message': 'تیم مورد نظر پیدا نشد.'}, status=HTTP_404_NOT_FOUND)
+    if request.user.team is None:
+        return Response({'message': 'شما هنوز عضو تیمی نشده‌اید.'}, status=HTTP_403_FORBIDDEN)
+    sender = request.user.team
+    game_request = GameRequest(sender=sender, receiver=receiver)
+    game_request.save()
+    return Response({'message': 'درخواست بازی دوستانه با موفقیت ارسال شد.'}, status=HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def accept_game_request(request):
+    try:
+        game_request = GameRequest.objects.get(pk=int(request.data.get('request_id')))
+    except ValueError:
+        return Response({'message': 'شناسه درخواست بازی نامعتبر است.'}, status=HTTP_400_BAD_REQUEST)
+    except GameRequest.DoesNotExist:
+        return Response({'message': 'درخواست مورد نظر پیدا نشد.'}, status=HTTP_404_NOT_FOUND)
+    if request.user.team != game_request.receiver:
+        return Response({'message': 'شما قادر به تایید این بازی نیستید.'}, status=HTTP_403_FORBIDDEN)
+    game_request.status = GameRequest.ACCEPTED
+    game_request.save()
+    game = Game(request=game_request)
+    game.save()
+    game.add_to_queue()
+    return Response({'message': 'درخواست بازی تایید شد.'}, status=HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def reject_game_request(request):
+    try:
+        game_request = GameRequest.objects.get(pk=int(request.data.get('request_id')))
+    except ValueError:
+        return Response({'message': 'شناسه درخواست بازی نامعتبر است.'}, status=HTTP_400_BAD_REQUEST)
+    except GameRequest.DoesNotExist:
+        return Response({'message': 'درخواست مورد نظر پیدا نشد.'}, status=HTTP_404_NOT_FOUND)
+    if request.user.team != game_request.receiver:
+        return Response({'message': 'شما قادر به رد این بازی نیستید.'}, status=HTTP_403_FORBIDDEN)
+    game_request.status = GameRequest.REJECTED
+    game_request.save()
+    return Response({'message': 'درخواست بازی رد شد.'}, status=HTTP_200_OK)
+
+
+@api_view(['POST'])
+def callback_update_game_status(request):
+    pass
