5ac80e4160fd8e879c22158d37a62cdcaef28c9f
Parham Kazemi <pkazemi3@gmail.com>
2018-11-20 18:29:15 +0330

    Fixed game connection server error

--- game_manager/server_api.py
+++ game_manager/server_api.py
@@ -3,15 +3,14 @@ import requests
 from uiai2018_site.settings import GAME_RUNNER_SERVERS
 
 
-def get_server_status(server_address):
-    r = requests.get('{}/api/server/status/'.format(server_address))
-    return r.json()
-
-
 def get_best_server():
     for server in GAME_RUNNER_SERVERS:
-        if get_server_status(server)['code'] == 200:
-            return server
+        r = requests.get('{}/api/server/status/'.format(server))
+        try:
+            if r.json()['code'] == 200:
+                return server
+        except requests.exceptions.ConnectionError:
+            pass
 
 
 def request_game_run(client1_name, client1_path, client1_language,
