653253eba4c34ec4b5b8e6540246030e376f1ebe
Parham Kazemi <pkazemi3@gmail.com>
2018-11-27 20:22:04 +0330

    Updated update script

--- reset_services.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-service uwsgi restart
-service nginx restart
--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
+sudo service uwsgi restart
+sudo service nginx restart
