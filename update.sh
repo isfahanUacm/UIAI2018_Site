8879e8ac24b5c9b5368c9a18fac94bd24087b7dc
Parham Kazemi <pkazemi3@gmail.com>
2018-11-27 20:03:34 +0330

    Fixed update script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git pull
-pip install -r requirements
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
