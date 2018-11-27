89f73f7d073e73fef33556a8ed28a32238592891
Parham Kazemi <pkazemi3@gmail.com>
2018-11-27 20:24:50 +0330

    Updated update script

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "PULLING FROM GIT..."
 git pull
+echo "INSTALLING REQUIREMENTS..."
 pip install -r requirements.txt
+echo "RUNNING DJANGO MIGRATIONS..."
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
+echo "RESTARTING SERVICES..."
 sudo service uwsgi restart
 sudo service nginx restart
