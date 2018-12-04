6401b5a68d9f934545fac7c9f857150a6c369d62
Parham Kazemi <pkazemi3@gmail.com>
2018-12-04 15:24:37 +0330

    Updated fix qual script

--- game_manager/management/commands/fix_qualifications.py
+++ game_manager/management/commands/fix_qualifications.py
@@ -13,4 +13,5 @@ class Command(BaseCommand):
         for i, game in enumerate(games):
             print('Adding {}: {} vs {}'.format(game.pk, game.get_request_sender_team().name,
                                                game.get_request_receiver_team().name))
-            tasks.add_game_to_queue(game.pk, 'acm.ui.ac.ir/' + reverse('callback_game_status'), schedule=i * 5)
+            t = (i // 8) * 10
+            tasks.add_game_to_queue(game.pk, 'acm.ui.ac.ir/' + reverse('callback_game_status'), schedule=t)
