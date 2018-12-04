2c93a339410ec8a54e1924d21a2e67fe46061024
Parham Kazemi <pkazemi3@gmail.com>
2018-12-04 17:50:20 +0330

    Updated qual game running script

--- game_manager/management/commands/fix_qualifications.py
+++ game_manager/management/commands/fix_qualifications.py
@@ -5,13 +5,26 @@ from django.urls import reverse
 
 from game_manager.models import *
 from game_manager import server_api, tasks
+from game_manager.server_api import request_game_run
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         games = Game.objects.filter(game_type=Game.QUALIFICATION, status=Game.PLAYING)
         for i, game in enumerate(games):
-            t = (i // 8 + 1) * 10
-            print('t = {}; {}: {} vs {}'.format(t, game.pk, game.get_request_sender_team().name,
+            if i % 8 == 7:
+                print('WAITING...')
+                time.sleep(30)
+            print('RUNNING {}: {} vs {}'.format(game.pk, game.get_request_sender_team().name,
                                                 game.get_request_receiver_team().name))
-            tasks.add_game_to_queue(game.pk, 'acm.ui.ac.ir/' + reverse('callback_game_status'), schedule=t)
+            request_game_run(
+                client1_name=game.get_request_sender_team().name,
+                client1_language=game.get_request_sender_team().get_final_code().language,
+                client1_path=game.get_request_sender_team().get_final_code().code_zip.path,
+                client2_name=game.get_request_receiver_team().name,
+                client2_language=game.get_request_receiver_team().get_final_code().language,
+                client2_path=game.get_request_receiver_team().get_final_code().code_zip.path,
+                game_id=game.pk,
+                token=game.token,
+                callback_url='http://' + 'acm.ui.ac.ir/' + reverse('callback_game_status'),
+            )
