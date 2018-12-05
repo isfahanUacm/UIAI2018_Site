d203266699e1caec4e64cec8f47b5fb659ed3f3e
Parham Kazemi <pkazemi3@gmail.com>
2018-12-05 13:19:52 +0330

    Added manual payment verification script

---
+++ user_panel/management/commands/verify_payment.py
@@ -0,0 +1,17 @@
+import requests
+from django.core.management import BaseCommand
+
+from uiai2018_site.settings import PAYMENT_API_KEY
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        transaction_id = int(input("Enter transaction ID: "))
+        data = {
+            "api": PAYMENT_API_KEY,
+            "transId": transaction_id
+        }
+        response = requests.post('https://pay.ir/payment/verify', data=data)
+        print('Response Code: {}'.format(response.status_code))
+        print('Response Data: {}'.format(response.json().decode('utf-8')))
