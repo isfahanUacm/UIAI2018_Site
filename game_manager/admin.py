bdbef238304bbccb2f88bf344fb4ef99ce7701c0
Parham Kazemi <pkazemi3@gmail.com>
2018-12-06 13:03:51 +0330

    Added link href to game admin

--- game_manager/admin.py
+++ game_manager/admin.py
@@ -49,7 +49,7 @@ class RequestAdmin(admin.ModelAdmin):
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'get_request_sender_team', 'get_request_receiver_team', 'status', 'game_type',
-                    'get_result_string', 'get_log_link_html']
+                    'get_result_string', 'get_log_url']
     list_filter = ['status', 'game_type']
     actions = [start_games, set_status_playing, set_status_waiting, set_status_error]
 
