d51f7ef5d4b4267527530b41b53c758db6106180
Parham Kazemi <pkazemi3@gmail.com>
2018-12-06 07:04:27 +0330

    Updated print team info script

--- user_panel/management/commands/print_teams.py
+++ user_panel/management/commands/print_teams.py
@@ -7,22 +7,11 @@ from user_panel.models import *
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        qualified_only = (input("Qualified only? [y/n] ").lower() == 'y')
-        paid_only = False
-        if qualified_only:
-            paid_only = (input("Paid only? [y/n] ").lower() == 'y')
-        teams = Team.objects.all()
-        if qualified_only:
-            teams = teams.filter(qualified=True)
-        if paid_only:
-            teams = teams.filter(payment_verified=True)
+        teams = Team.objects.filter(qualified=True, payment_verified=True)
         teams = sorted(teams, key=lambda t: t.name)
-        print("TEAM NAME,PAYMENT VERIFIED,FIRST NAME (1),LAST NAME (1),EMAIL (1),PHONE (1),INSTITUTE (1)," +
-              "FIRST NAME (2),LAST NAME (2),EMAIL (2),PHONE (2), INSTITUTE (2)," +
-              "FIRST NAME (3),LAST NAME (3),EMAIL (3),PHONE (3), INSTITUTE (3)")
+        print("TEAM NAME,PAYMENT VERIFIED,FIRST NAME,LAST NAME,EMAIL,PHONE,INSTITUTE")
         for team in teams:
             if team.pk not in TEST_TEAM_PKS:
-                s = "{},{}".format(team.name, "YES" if team.payment_verified else "NO")
-                for m in team.members.all()[:3]:
-                    s += ",{},{},{},{},{}".format(m.first_name, m.last_name, m.email, m.phone, m.institute)
-                print(s)
+                for member in team.members.all()[:3]:
+                    print("{},{},{},{},{},{},{}".format(team.name, team.payment_verified, member.first_name,
+                                                        member.last_name, member.email, member.phone, member.institute))
